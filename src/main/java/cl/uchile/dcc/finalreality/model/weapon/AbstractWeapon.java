37a0a5d7fe5d65999a812527225c119f3120bb18
overexpOG <ignacio.alvbah@gmail.com>
2022-09-24 23:06:07 -0300

    throws are added in abstract weapon

--- src/main/java/cl/uchile/dcc/finalreality/model/weapon/AbstractWeapon.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/weapon/AbstractWeapon.java
@@ -1,4 +1,15 @@
+/*
+ * "Final Reality" (c) by R8V and Ignacio Alveal
+ * "Final Reality" is licensed under a
+ * Creative Commons Attribution 4.0 International License.
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
 package cl.uchile.dcc.finalreality.model.weapon;
+
+import cl.uchile.dcc.finalreality.exceptions.InvalidStatValueException;
+import cl.uchile.dcc.finalreality.exceptions.Require;
 
 /**
  * A class that holds all the information of a weapon.
@@ -21,7 +32,9 @@ public abstract class AbstractWeapon implements Weapon{
    * @param weight
    *     the weapon's weight
    */
-  public AbstractWeapon(final String name, final int damage, final int weight) {
+  public AbstractWeapon(final String name, final int damage, final int weight) throws InvalidStatValueException {
+    Require.statValueAtLeast(0, weight, "Weight");
+    Require.statValueAtLeast(0, damage, "Damage");
     this.name = name;
     this.damage = damage;
     this.weight = weight;
