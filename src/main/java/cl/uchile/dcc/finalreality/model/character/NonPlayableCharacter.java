17ff8aca4cc99a69763d5db975b7e72af119b657
overexpOG <ignacio.alvbah@gmail.com>
2022-09-28 22:30:27 -0300

    create interface for NonPlayableCharacter.

---
+++ src/main/java/cl/uchile/dcc/finalreality/model/character/NonPlayableCharacter.java
@@ -0,0 +1,25 @@
+/*
+ * "Final Reality" (c) by R8V and Ignacio Alveal
+ * "Final Reality" is licensed under a
+ * Creative Commons Attribution 4.0 International License.
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+package cl.uchile.dcc.finalreality.model.character;
+
+import cl.uchile.dcc.finalreality.model.character.player.PlayerCharacter;
+
+/**
+ * A {@link GameCharacter} with weight (fight against {@link PlayerCharacter}).
+ *
+ * @author <a href="https://www.github.com/r8vnhill">R8V</a>
+ * @author Ignacio Alveal
+ */
+public interface NonPlayableCharacter extends GameCharacter{
+
+  /**
+   * Returns the weight of this enemy.
+   */
+  int getWeight();
+}
