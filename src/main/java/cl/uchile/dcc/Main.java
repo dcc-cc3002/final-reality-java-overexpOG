4a07a7b544bf8243a77524d7647de0a8bf0fc0bb
overexpOG <ignacio.alvbah@gmail.com>
2022-09-29 19:04:10 -0300

    improving the main.

--- src/main/java/cl/uchile/dcc/Main.java
+++ src/main/java/cl/uchile/dcc/Main.java
@@ -27,9 +27,9 @@ public class Main {
    * function that does something when the file is opened.
    */
   public static void main(String[] args) throws InterruptedException, InvalidStatValueException {
-    //create the queue
+    //create the queue.
     BlockingQueue<GameCharacter> queue = new LinkedBlockingQueue<>();
-    //create the characters and weapons to see that the constructors work (also equip and waitTurn)
+    //create the characters and weapons to see that the constructors work (also equip and waitTurn).
     var knife = new Knife("wooden knife", 1, 1);
     var character1 = new Thief("Haruhiro", 10, 10, queue);
     character1.equip(knife);
@@ -54,36 +54,52 @@ public class Main {
     var character6 = new BlackMage("Shihoru", 10, 6, 10, queue);
     character6.equip(wizardStaff);
     character6.waitTurn();
-    var character7 = new Enemy("enemy", 7, 10, 10, queue);
+    var character7 = new Enemy("Jumbo", 7, 50, 20, queue);
     character7.waitTurn();
-    //testing AbstractCharacter methods
+    var character8 = new Enemy("Arnold", 8, 70, 10, queue);
+    character8.waitTurn();
+    //testing AbstractCharacter methods.
     System.out.println("testing AbstractCharacter methods:");
-    System.out.println(character1.getName());
-    System.out.println(character1.getMaxHp());
+    System.out.println(character1);
+    System.out.println("name = " + character1.getName());
+    System.out.println("maxHp = " + character1.getMaxHp());
+    System.out.println("currentHp = " + character1.getCurrentHp());
+    System.out.println("defense = " + character1.getDefense());
     character1.setCurrentHp(8);
-    System.out.println(character1.getCurrentHp());
-    System.out.println(character1.getDefense());
-    //testing AbstractMage methods
+    System.out.println("change the currentHp from 10 to 8");
+    System.out.println(character1);
+    //testing AbstractMage methods.
     System.out.println("testing AbstractMage methods:");
-    System.out.println(character3.getMaxMp());
+    System.out.println(character3);
+    System.out.println("maxMp = " + character3.getMaxMp());
+    System.out.println("currentMp = " + character3.getCurrentMp());
     character3.setCurrentMp(8);
-    System.out.println(character3.getCurrentMp());
-    //testing AbstractWeapon methods
+    System.out.println("change the currentMp from 10 to 8");
+    System.out.println(character3);
+    //testing AbstractWeapon methods.
     System.out.println("testing AbstractWeapon methods:");
-    System.out.println(wizardStaff.getName());
-    System.out.println(wizardStaff.getWeight());
-    System.out.println(wizardStaff.getDamage());
-    //testing getType methods of all weapons
+    //this method is not from Abstract, but it allows an easier visualization.
+    System.out.println(wizardStaff);
+    System.out.println("name = " + wizardStaff.getName());
+    System.out.println("weight = " + wizardStaff.getWeight());
+    System.out.println("damage = " + wizardStaff.getDamage());
+    //testing methods of all weapons.
     System.out.println("testing getType methods of all weapons:");
-    System.out.println(knife.getType());
-    System.out.println(bow.getType());
-    System.out.println(healerStaff.getType());
-    System.out.println(sword.getType());
-    System.out.println(axe.getType());
-    //testins AbstractPlaterCharacter methods
-    System.out.println("testins AbstractPlaterCharacter methods:");
-    System.out.println(character6.getEquippedWeapon().getName());
-    //testing toString and equals of weapons (only one is done because the rest are equivalent)
+    System.out.println(knife);
+    System.out.println("knife.getType() = " + knife.getType());
+    System.out.println(bow);
+    System.out.println("bow.getType() = " + bow.getType());
+    System.out.println(healerStaff);
+    System.out.println("healerStaff.getType() = " + healerStaff.getType());
+    System.out.println(sword);
+    System.out.println("sword.getType() = " + sword.getType());
+    System.out.println(axe);
+    System.out.println("axe.getType() = " + axe.getType());
+    //testins AbstractPlayerCharacter methods (equip is already tested before and
+    // waitTurn will be tested after).
+    System.out.println("testins AbstractPlayerCharacter methods:");
+    System.out.println(character6.getEquippedWeapon());
+    //testing toString and equals of weapons (only one is done because the rest are equivalent).
     System.out.println("testing toString and equals of weapons "
             + "(only one is done because the rest are equivalent):");
     var wizardStaffX = new Staff("wizard staff", 6, 6);
@@ -92,9 +108,9 @@ public class Main {
     System.out.println(healerStaff);
     System.out.println("wizard_staff.equals(healer_staff) = "
             + (wizardStaff.equals(healerStaff)));
-    System.out.println("wizard_staff.equals(wizard_staff_x) = "
+    System.out.println("wizard_staff.equals(wizard_staffX) = "
             + (wizardStaff.equals(wizardStaffX)));
-    //testing equals of characters (only one is done because the rest are equivalent)
+    //testing equals of characters (only one is done because the rest are equivalent).
     System.out.println("testing equals of characters "
             + "(only one is done because the rest are equivalent):");
     var character2X = new Engineer("Yume", 10, 8, queue);
@@ -102,13 +118,22 @@ public class Main {
     System.out.println(character2X);
     System.out.println(character5);
     System.out.println("character2.equals(character5) = " + (character2.equals(character5)));
-    System.out.println("character2.equals(character2_x) = " + (character2.equals(character2X)));
-    // Waits for 10 seconds to ensure that all characters have finished waiting
-    System.out.println("testing toString of characters and queue:");
-    Thread.sleep(10000);
+    System.out.println("character2.equals(character2X) = " + (character2.equals(character2X)));
+    //testing equals of enemy.
+    System.out.println("testing equals of enemy:");
+    var character7X = new Enemy("Jumbo", 7, 50, 20, queue);
+    System.out.println(character7);
+    System.out.println(character7X);
+    System.out.println(character8);
+    System.out.println("character7.equals(character8) = " + (character7.equals(character8)));
+    System.out.println("character7.equals(character7X) = " + (character7.equals(character7X)));
+    // Waits for 6 seconds to ensure that all characters have finished waiting.
+    System.out.println("testing toString of characters and waitTurn of "
+            + "AbstractPlayerCharacter and AbstractNonPlayableCharacter:");
+    Thread.sleep(6000);
     while (!queue.isEmpty()) {
       // Pops and prints the names of the characters of the queue to illustrate the turns
-      // order (to see that the toString work)
+      // order (to see that the toString work).
       System.out.println(queue.poll().toString());
     }
   }
