7519613e68299828cab13171c4c36dafb13ec279
overexpOG <ignacio.alvbah@gmail.com>
2022-09-28 22:34:49 -0300

    adding the method waitTurn for PlayerCharacter.

--- src/main/java/cl/uchile/dcc/finalreality/model/character/player/AbstractPlayerCharacter.java
+++ src/main/java/cl/uchile/dcc/finalreality/model/character/player/AbstractPlayerCharacter.java
@@ -9,10 +9,13 @@
 package cl.uchile.dcc.finalreality.model.character.player;
 
 import cl.uchile.dcc.finalreality.exceptions.InvalidStatValueException;
-import cl.uchile.dcc.finalreality.model.character.AbstractAddingQueue;
+import cl.uchile.dcc.finalreality.model.character.AbstractCharacter;
 import cl.uchile.dcc.finalreality.model.character.GameCharacter;
 import cl.uchile.dcc.finalreality.model.weapon.Weapon;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -25,10 +28,14 @@ import org.jetbrains.annotations.NotNull;
  * @author <a href="https://www.github.com/r8vnhill">R8V</a>
  * @author Ignacio Alveal
  */
-public abstract class AbstractPlayerCharacter extends AbstractAddingQueue implements
+public abstract class AbstractPlayerCharacter extends AbstractCharacter implements
         PlayerCharacter {
 
   private Weapon equippedWeapon = null;
+
+  protected final BlockingQueue<GameCharacter> turnsQueue;
+
+  private ScheduledExecutorService scheduledExecutor;
 
   /**
    * Creates a new character.
@@ -46,7 +53,8 @@ public abstract class AbstractPlayerCharacter extends AbstractAddingQueue implem
   protected AbstractPlayerCharacter(@NotNull final String name, final int maxHp, final int defense,
                                     @NotNull final BlockingQueue<GameCharacter> turnsQueue)
           throws InvalidStatValueException {
-    super(name, maxHp, defense, turnsQueue);
+    super(name, maxHp, defense);
+    this.turnsQueue = turnsQueue;
   }
 
   @Override
@@ -57,5 +65,27 @@ public abstract class AbstractPlayerCharacter extends AbstractAddingQueue implem
   @Override
   public Weapon getEquippedWeapon() {
     return equippedWeapon;
+  }
+
+  /**
+   * Adds this character to the turns queue.
+   */
+  private void addToQueue() {
+    try {
+      turnsQueue.put(this);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    scheduledExecutor.shutdown();
+  }
+
+  @Override
+  public void waitTurn() {
+    scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
+    var player =  this;
+    scheduledExecutor.schedule(
+            /* command = */ this::addToQueue,
+            /* delay = */ player.getEquippedWeapon().getWeight() / 10,
+            /* unit = */ TimeUnit.SECONDS);
   }
 }
