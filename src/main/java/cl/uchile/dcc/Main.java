bda0a0a8ea9b52118fc227c89524acc3b526ec0e
overexpOG <ignacio.alvbah@gmail.com>
2022-09-29 23:52:01 -0300

    improving the main.

--- src/main/java/cl/uchile/dcc/Main.java
+++ src/main/java/cl/uchile/dcc/Main.java
@@ -47,7 +47,7 @@ public class Main {
     character4.equip(sword);
     character4.waitTurn();
     var axe = new Axe("wooden axe", 5, 5);
-    var character5 = new Engineer("Kiichi", 5, 5, queue);
+    var character5 = new Engineer("Kiichi", 10, 8, queue);
     character5.equip(axe);
     character5.waitTurn();
     var wizardStaff = new Staff("wizard staff", 6, 6);
@@ -99,9 +99,44 @@ public class Main {
     // waitTurn will be tested after).
     System.out.println("testins AbstractPlayerCharacter methods:");
     System.out.println(character6.getEquippedWeapon());
-    //testing toString and equals of weapons (only one is done because the rest are equivalent).
-    System.out.println("testing toString and equals of weapons "
-            + "(only one is done because the rest are equivalent):");
+    //testing toString and equals of weapons.
+    System.out.println("testing toString and equals of weapons:");
+    var knifeX = new Knife("wooden knife", 1, 1);
+    var knifeY = new Knife("metal knife", 4, 5);
+    System.out.println(knife);
+    System.out.println(knifeX);
+    System.out.println(knifeY);
+    System.out.println("knife.equals(knifeX) = "
+            + (knife.equals(knifeX)));
+    System.out.println("knife.equals(knifeY) = "
+            + (knife.equals(knifeY)));
+    var bowX = new Bow("wooden bow", 2, 2);
+    var bowY = new Bow("fire bow", 2, 2);
+    System.out.println(bow);
+    System.out.println(bowX);
+    System.out.println(bowY);
+    System.out.println("bow.equals(bowX) = "
+            + (bow.equals(bowX)));
+    System.out.println("bow.equals(bowY) = "
+            + (bow.equals(bowY)));
+    var swordX = new Sword("wooden sword", 4, 4);
+    var swordY = new Sword("stone sword", 4, 4);
+    System.out.println(sword);
+    System.out.println(swordX);
+    System.out.println(swordY);
+    System.out.println("sword.equals(swordX) = "
+            + (sword.equals(swordX)));
+    System.out.println("sword.equals(swordY) = "
+            + (sword.equals(swordY)));
+    var axeX = new Axe("wooden axe", 5, 5);
+    var axeY = new Axe("wooden axe", 6, 5);
+    System.out.println(axe);
+    System.out.println(axeX);
+    System.out.println(axeY);
+    System.out.println("axe.equals(axeX) = "
+            + (axe.equals(axeX)));
+    System.out.println("axe.equals(axeY) = "
+            + (axe.equals(axeY)));
     var wizardStaffX = new Staff("wizard staff", 6, 6);
     System.out.println(wizardStaff);
     System.out.println(wizardStaffX);
@@ -110,15 +145,42 @@ public class Main {
             + (wizardStaff.equals(healerStaff)));
     System.out.println("wizard_staff.equals(wizard_staffX) = "
             + (wizardStaff.equals(wizardStaffX)));
-    //testing equals of characters (only one is done because the rest are equivalent).
-    System.out.println("testing equals of characters "
-            + "(only one is done because the rest are equivalent):");
+    //testing equals of characters.
+    System.out.println("testing equals of characters:");
+    var character1X = new Thief("Haruhiro", 10, 10, queue);
+    var character1Y = new Thief("Haruhiro", 8, 10, queue);
+    System.out.println(character1);
+    System.out.println(character1X);
+    System.out.println(character1Y);
+    System.out.println("character1.equals(character1X) = " + (character1.equals(character1X)));
+    System.out.println("character1.equals(character1Y) = " + (character1.equals(character1Y)));
     var character2X = new Engineer("Yume", 10, 8, queue);
     System.out.println(character2);
     System.out.println(character2X);
     System.out.println(character5);
     System.out.println("character2.equals(character5) = " + (character2.equals(character5)));
     System.out.println("character2.equals(character2X) = " + (character2.equals(character2X)));
+    var character3X = new WhiteMage("Merry", 15, 5, 10, queue);
+    var character3Y = new WhiteMage("Merry", 15, 5, 8, queue);
+    System.out.println(character3);
+    System.out.println(character3X);
+    System.out.println(character3Y);
+    System.out.println("character3.equals(character3X) = " + (character3.equals(character3X)));
+    System.out.println("character3.equals(character3Y) = " + (character3.equals(character3Y)));
+    var character4X = new Knight("Ranta", 10, 20, queue);
+    var character4Y = new Knight("Ranta", 10, 15, queue);
+    System.out.println(character4);
+    System.out.println(character4X);
+    System.out.println(character4Y);
+    System.out.println("character4.equals(character4X) = " + (character4.equals(character4X)));
+    System.out.println("character4.equals(character4Y) = " + (character4.equals(character4Y)));
+    var character6X = new BlackMage("Shihoru", 10, 6, 10, queue);
+    var character6Y= new BlackMage("Shihoru", 10, 2, 10, queue);
+    System.out.println(character6);
+    System.out.println(character6X);
+    System.out.println(character6Y);
+    System.out.println("character6.equals(character6X) = " + (character6.equals(character6X)));
+    System.out.println("character6.equals(character6Y) = " + (character6.equals(character6Y)));
     //testing equals of enemy.
     System.out.println("testing equals of enemy:");
     var character7X = new Enemy("Jumbo", 7, 50, 20, queue);
