e39702e9b4191d1ac535f81e51d469f00279f76f
overexpOG <ignacio.alvbah@gmail.com>
2022-10-20 18:36:48 -0300

    create WhiteMageTest.

---
+++ src/test/java/cl/uchile/dcc/finalreality/model/character/player/WhiteMageTest.java
@@ -0,0 +1,40 @@
+package cl.uchile.dcc.finalreality.model.character.player;
+
+import cl.uchile.dcc.finalreality.model.character.GameCharacter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WhiteMageTest {
+  private BlockingQueue<GameCharacter> queue;
+  private Mage character1;
+  private Mage character2;
+  private Mage character3;
+  private Mage character4;
+
+  @BeforeEach
+  void setUp() {
+    queue = new LinkedBlockingQueue<>();
+    character1 = new WhiteMage("Merry", 15, 5, 10, queue);
+    character2 = new WhiteMage("Merry", 15, 5, 10, queue);
+    character3 = new WhiteMage("Merry", 8, 5, 10, queue);
+    character4 = new BlackMage("Shihoru", 10, 6, 10, queue);
+  }
+
+  @Test
+  void testToString() {
+    assertEquals("WhiteMage{name='Merry', maxHp=15, currentHp=15, defense=5, maxMp=10, currentMp=10}", character1.toString());
+  }
+
+  @Test
+  void testEquals() {
+    assert (character1.equals(character1));
+    assert (character1.equals(character2));
+    assertFalse (character1.equals(character3));
+    assertFalse (character1.equals(character4));
+  }
+}
