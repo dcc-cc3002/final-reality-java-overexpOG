9c7efc7d3dcb8b9f299e8364876315d0d03a86eb
overexpOG <ignacio.alvbah@gmail.com>
2022-10-20 18:57:10 -0300

    create AbstractNonPlayableCharacterTest.

---
+++ src/test/java/cl/uchile/dcc/finalreality/model/character/nonplayable/AbstractNonPlayableCharacterTest.java
@@ -0,0 +1,31 @@
+package cl.uchile.dcc.finalreality.model.character.nonplayable;
+
+import cl.uchile.dcc.finalreality.model.character.GameCharacter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractNonPlayableCharacterTest {
+  private BlockingQueue<GameCharacter> queue;
+  private NonPlayableCharacter character1;
+
+  @BeforeEach
+  void setUp() {
+    queue = new LinkedBlockingQueue<>();
+    character1 = new Enemy("Jumbo", 7, 50, 20, queue);
+  }
+
+  @Test
+  void getWeight() {
+    assertEquals (7, character1.getWeight());
+  }
+
+  @Test
+  void waitTurn() {
+    character1.waitTurn();
+  }
+}
