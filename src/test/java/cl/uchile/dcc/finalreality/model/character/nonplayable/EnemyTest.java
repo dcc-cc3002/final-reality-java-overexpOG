44398f35808140adf0cf139fbfe9617d64661e4e
overexpOG <ignacio.alvbah@gmail.com>
2022-10-25 19:15:49 -0300

    adding the author.

--- src/test/java/cl/uchile/dcc/finalreality/model/character/nonplayable/EnemyTest.java
+++ src/test/java/cl/uchile/dcc/finalreality/model/character/nonplayable/EnemyTest.java
@@ -1,3 +1,11 @@
+/*
+ * "Final Reality" (c) by R8V and Ignacio Alveal
+ * "Final Reality" is licensed under a
+ * Creative Commons Attribution 4.0 International License.
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
 package cl.uchile.dcc.finalreality.model.character.nonplayable;
 
 import cl.uchile.dcc.finalreality.model.character.GameCharacter;
