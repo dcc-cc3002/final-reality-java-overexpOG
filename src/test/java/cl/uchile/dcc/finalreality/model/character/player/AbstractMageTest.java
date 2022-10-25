ce80949019783fb34adb97cc05c213fddde3905a
overexpOG <ignacio.alvbah@gmail.com>
2022-10-25 18:47:01 -0300

    adding edge cases to AbstractMageTest.

--- src/test/java/cl/uchile/dcc/finalreality/model/character/player/AbstractMageTest.java
+++ src/test/java/cl/uchile/dcc/finalreality/model/character/player/AbstractMageTest.java
@@ -1,5 +1,6 @@
 package cl.uchile.dcc.finalreality.model.character.player;
 
+import cl.uchile.dcc.finalreality.exceptions.InvalidStatValueException;
 import cl.uchile.dcc.finalreality.model.character.GameCharacter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,10 +12,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AbstractMageTest {
   private Mage character1;
+  private BlockingQueue<GameCharacter> queue;
 
   @BeforeEach
   void setUp() {
-    BlockingQueue<GameCharacter> queue = new LinkedBlockingQueue<>();
+    queue = new LinkedBlockingQueue<>();
     character1 = new WhiteMage("Merry", 15, 5, 10, queue);
   }
 
@@ -27,10 +29,35 @@ class AbstractMageTest {
   void setCurrentMp() {
     character1.setCurrentMp(8);
     assertEquals(8, character1.getCurrentMp());
+    boolean currentMpNegativo = true;
+    try {
+      character1.setCurrentMp(-2);
+    } catch (InvalidStatValueException e){
+      currentMpNegativo = false;
+    }
+    assertEquals (false, currentMpNegativo);
+    boolean currentMpMayorAMaxMp = true;
+    try {
+      character1.setCurrentMp(11);
+    } catch (InvalidStatValueException e){
+      currentMpMayorAMaxMp = false;
+    }
+    assertEquals (false, currentMpMayorAMaxMp);
   }
 
   @Test
   void getMaxMp() {
     assertEquals(10, character1.getMaxMp());
+  }
+
+  @Test
+  void Constructor() {
+    boolean MaxMpNegativoOCero = true;
+    try {
+      new BlackMage("Shihoru", 10, 6, 0, queue);
+    } catch (InvalidStatValueException e){
+      MaxMpNegativoOCero = false;
+    }
+    assertEquals (false, MaxMpNegativoOCero);
   }
 }
