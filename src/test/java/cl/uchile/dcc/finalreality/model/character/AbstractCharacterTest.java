52434e80dc3061c1a4c4cfc9c28f6e2a496dd7f1
overexpOG <ignacio.alvbah@gmail.com>
2022-10-25 18:42:11 -0300

    adding edge cases to AbstractCharacterTest.

--- src/test/java/cl/uchile/dcc/finalreality/model/character/AbstractCharacterTest.java
+++ src/test/java/cl/uchile/dcc/finalreality/model/character/AbstractCharacterTest.java
@@ -1,5 +1,7 @@
 package cl.uchile.dcc.finalreality.model.character;
 
+import cl.uchile.dcc.finalreality.exceptions.InvalidStatValueException;
+import cl.uchile.dcc.finalreality.model.character.player.Knight;
 import cl.uchile.dcc.finalreality.model.character.player.Thief;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,10 +12,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AbstractCharacterTest {
   private GameCharacter character1;
+  private BlockingQueue<GameCharacter> queue;
 
   @BeforeEach
   void setUp() {
-    BlockingQueue<GameCharacter> queue = new LinkedBlockingQueue<>();
+    queue = new LinkedBlockingQueue<>();
     character1 = new Thief("Haruhiro", 10, 10, queue);
   }
 
@@ -41,5 +44,36 @@ class AbstractCharacterTest {
   void setCurrentHp() {
     character1.setCurrentHp(8);
     assertEquals(8, character1.getCurrentHp());
+    boolean currentHpNegativo = true;
+    try {
+      character1.setCurrentHp(-2);
+    } catch (InvalidStatValueException e){
+      currentHpNegativo = false;
+    }
+    assertEquals (false, currentHpNegativo);
+    boolean currentHpMayorAMaxHp = true;
+    try {
+      character1.setCurrentHp(11);
+    } catch (InvalidStatValueException e){
+      currentHpMayorAMaxHp = false;
+    }
+    assertEquals (false, currentHpMayorAMaxHp);
+  }
+  @Test
+  void Constructor() {
+    boolean MaxHpNegativoOCero = true;
+    try {
+      new Knight("Ranta", 0, 20, queue);
+    } catch (InvalidStatValueException e){
+      MaxHpNegativoOCero = false;
+    }
+    assertEquals (false, MaxHpNegativoOCero);
+    boolean defenseNegativo = true;
+    try {
+      new Knight("Ranta", 10, -2, queue);
+    } catch (InvalidStatValueException e){
+      defenseNegativo = false;
+    }
+    assertEquals (false, defenseNegativo);
   }
 }
